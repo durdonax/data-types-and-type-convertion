35534c8c71d412e46fcbe66cef1ba92b7fdab2ee
durdonax <durdonasheykhadx@gmail.com>
2024-12-09 16:51:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let string = prompt("String typedagi ma'lumot kiriting?")
+let num = +prompt("Number typedagi ma'lumot kiriting?")
+let lean = prompt("Boolean typedagi ma'lumot kiriting?")
+alert(string +" "+ num +" "+ lean )
+confirm("Hamma ma'lumotlarni kiriganingizdan ishonchingiz komilmi?")
+
+console.log(string +" "+ num +" "+ lean)
